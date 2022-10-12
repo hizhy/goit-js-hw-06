d5fff92ec47b5f72c06ee8e692587f7ad235ddea
Mykola_Kh <hizhym@gmail.com>
2022-10-12 16:20:32 +0300

    task-02 finished

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const markUp = ingredients.map((el) => {
+  const itemRef = document.createElement("li");
+  itemRef.textContent = el;
+  itemRef.classList.add("item");
+  return itemRef;
+});
+
+const liRef = document.querySelector("#ingredients");
+liRef.append(...markUp);
