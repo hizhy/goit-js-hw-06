eb6407f1ac700c0faf04103312df717e9ecd568b
Mykola_Kh <hizhym@gmail.com>
2022-10-16 00:02:53 +0300

    task-06 finished + add some styles

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputRef = document.querySelector("#validation-input");
+inputRef.addEventListener("blur", onInputBlur);
+function onInputBlur(event) {
+  if (
+    event.currentTarget.value.length ===
+    Number(inputRef.getAttribute("data-length"))
+  ) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+    return;
+  }
+  inputRef.classList.add("invalid");
+}
