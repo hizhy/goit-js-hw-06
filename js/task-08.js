ec69325516e73326a9b673e421e2b91e585b5303
Mykola_Kh <hizhym@gmail.com>
2022-10-17 00:53:21 +0300

    task-08 finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+const dataInFormRef = {};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (
+    !event.currentTarget.elements.email.value ||
+    !event.currentTarget.elements.password.value
+  ) {
+    alert("All fields must be completed!");
+  }
+
+  dataInFormRef.email = event.currentTarget.elements.email.value;
+  dataInFormRef.password = event.currentTarget.elements.password.value;
+
+  console.log(dataInFormRef);
+
+  event.currentTarget.reset();
+}
