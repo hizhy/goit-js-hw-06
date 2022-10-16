b157e8cc6205d737d877429158de0315a0c5c56f
Mykola_Kh <hizhym@gmail.com>
2022-10-17 01:40:48 +0300

    task-09 finished

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bgcRef = document.querySelector(".change-color");
+bgcRef.addEventListener("click", onChangeColorClick);
+
+const randomColorName = document.querySelector(".color");
+
+function onChangeColorClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  // console.log(document.body.style.backgroundColor);
+
+  randomColorName.textContent = document.body.style.backgroundColor;
+}
